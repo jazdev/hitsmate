33cdb4de91ef68b02d16741a54d40b78b085c621
Jasdev Singh <jasdev@live.in>
2014-10-01 13:12:20 +0530

    curve fitting now based on least error

--- hitsmate/hitsmate.py
+++ hitsmate/hitsmate.py
@@ -62,6 +62,8 @@ class HitsMateFrame(tk.Frame):
     randomWebLogsWindowOpenedFlag = False
     graphsWindowOpenedFlag = False
 
+
+
     def __init__(self, parent):
         """
             Initialize the parent Frame
@@ -70,6 +72,7 @@ class HitsMateFrame(tk.Frame):
         tk.Frame.__init__(self, parent)            
         self.parent = parent
         self.initUI()
+
 
 
     def initUI(self):
@@ -107,6 +110,7 @@ class HitsMateFrame(tk.Frame):
         global PredictFutureButton
         PredictFutureButton = tk.Button(self.parent, text = "Predict Future Traffic", command = self.predictFutureTraffic, bg="blue", fg="white")
         PredictFutureButton.place(x = 420, y = 280, width = 200, height = 30)
+
         
 
     def genRandomWebLogs(self):
@@ -136,7 +140,7 @@ class HitsMateFrame(tk.Frame):
             y += gamma.rvs(15, loc=0, scale=100, size=len(x))
             y += 2 * sp.exp(x / 100.0)
             y = sp.ma.array(y, mask=[y < 0])
-            sp.savetxt(os.path.join("hitsmate/sample_data", "sample_web_traffic.tsv"), list(zip(x, y)), delimiter="\t", fmt="%s")
+            sp.savetxt(os.path.join("sample_data", "sample_web_traffic.tsv"), list(zip(x, y)), delimiter="\t", fmt="%s")
             model = TableModel() # create a new TableModel for table data
             table = TableCanvas(RandomWebLogsWindow, model=model, editable=False) # create a new TableCanvas for showing the table
             table.createTableFrame()
@@ -150,7 +154,7 @@ class HitsMateFrame(tk.Frame):
             table.redrawTable()
 
         else:
-        	RandomWebLogsWindow.deiconify()  
+            RandomWebLogsWindow.deiconify()  
 
 
 
@@ -162,32 +166,44 @@ class HitsMateFrame(tk.Frame):
         linestyles = ['-', '--']
 
         def plot_models(x, y, models, fname):
-		    plt.clf()
-		    plt.scatter(x, y, s=10)
-		    plt.title("Last month's web traffic")
-		    plt.xlabel("Time")
-		    plt.ylabel("Hits / Hour")
-		    plt.xticks([week * 7 * 24 for week in range(10)], ['Week %i' % week for week in range(10)])
-		    mx = sp.linspace(0, x[-1], 1000)
-		    for model, style, color in zip(models, linestyles, colors):
-		        plt.plot(mx, model(mx), linestyle=style, linewidth=2, c=color)
+            plt.clf()
+            plt.scatter(x, y, s=10)
+            plt.title("Last month's web traffic")
+            plt.xlabel("Time")
+            plt.ylabel("Hits / Hour")
+            plt.xticks([week * 7 * 24 for week in range(10)], ['Week %i' % week for week in range(10)])
+            mx = sp.linspace(0, x[-1], 1000)
+            for model, style, color in zip(models, linestyles, colors):
+                plt.plot(mx, model(mx), linestyle=style, linewidth=2, c=color)
 
-		    plt.legend(["Degree=%i" % m.order for m in models], loc="upper left")
-		    plt.autoscale(tight=True)
-		    plt.ylim(ymin=0)
-		    plt.grid(True, linestyle='-', color='0.75')
-		    plt.savefig(fname)
-		    return
+            plt.legend(["Degree=%i" % m.order for m in models], loc="upper left")
+            plt.autoscale(tight=True)
+            plt.ylim(ymin=0)
+            plt.grid(True, linestyle='-', color='0.75')
+            plt.savefig(fname)
+            return
 
-        data = sp.genfromtxt("hitsmate/sample_data/sample_web_traffic.tsv", delimiter="\t")
+        data = sp.genfromtxt("sample_data/sample_web_traffic.tsv", delimiter="\t")
         x = data[:, 0]
         y = data[:, 1]
         print("Number of invalid entries:", sp.sum(sp.isnan(y)))
         x = x[~sp.isnan(y)]
         y = y[~sp.isnan(y)]
-        f2 = sp.poly1d(sp.polyfit(x, y, 2))
-        f3 = sp.poly1d(sp.polyfit(x, y, 3))
-        plot_models(x, y, [f2, f3], os.path.join("hitsmate/graphs", "Curve_fit_2nd_3rd.png"))
+        fp2, residuals2, rank2, sv2, rcond2 = sp.polyfit(x, y, 2, full=True)
+        fp3, residuals3, rank3, sv3, rcond3 = sp.polyfit(x, y, 3, full=True)
+        print "Error 2: ", str(residuals2)
+        print "Error 3: ", str(residuals3)
+        if residuals2 < residuals3:
+            print "Using 2nd degree model"
+            f2 = sp.poly1d(fp2)
+            print f2
+            plot_models(x, y, [f2], os.path.join("graphs", "Curve_fit.png"))
+        else:
+            print "Using 3rd degree model"
+            f3 = sp.poly1d(fp3)
+            print f3
+            plot_models(x, y, [f3], os.path.join("graphs", "Curve_fit.png"))
+        
         tkMessageBox.showinfo("Done", "Models successfully trained. Click 'Show Graphs' to see them.", parent = self.parent)
         return
 
@@ -211,15 +227,16 @@ class HitsMateFrame(tk.Frame):
             GraphsWindow = tk.Toplevel(self)
             GraphsWindow.minsize(300, 500)
             GraphsWindow.geometry("810x610+50+50")
-            GraphsWindow.title("Graph showing 2nd and 3rd degree curve fitted to the data")
+            GraphsWindow.title("Graph showing curve fitted to the data")
             GraphsWindow.config(bd=5)
             GraphsWindow.protocol("WM_DELETE_WINDOW", toggleFlag)
 
-            graph = Image.open("hitsmate/graphs/Curve_fit_2nd_3rd.png")
+            graph = Image.open("graphs/Curve_fit.png")
             graphPhoto = ImageTk.PhotoImage(graph)
             graphLabel = tk.Label(GraphsWindow, image=graphPhoto)
             graphLabel.image = graphPhoto 
             graphLabel.place(x = 0, y = 0, width = 800, height = 600)
+
 
 
     def predictFutureTraffic(self):
